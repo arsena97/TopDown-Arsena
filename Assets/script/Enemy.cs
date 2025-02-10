b2ac1f99a2860072b5e4473edfc9343640329e03
arsena97 <zivzivadzearsena72@gmail.com>
2025-02-10 20:23:35 +0400

    a

---
+++ Assets/script/Enemy.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] int hitCount = 3;
+    void OnTriggerEnter2D(Collider2D col)
+    {
+      
+        Destroy(gameObject);
+        Destroy(col.gameObject);
+    }
+
+}
