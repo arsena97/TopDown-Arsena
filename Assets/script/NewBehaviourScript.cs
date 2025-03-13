d2448d8e48b16564f99f6de559df810f017726c3
arsena97 <zivzivadzearsena72@gmail.com>
2025-03-13 20:31:47 +0400

    a

--- Assets/script/NewBehaviourScript.cs
+++ Assets/script/NewBehaviourScript.cs
@@ -5,52 +5,57 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    
-        [SerializeField] float speed = 9.5f;
-        [SerializeField] Camera camera;
+
+    [SerializeField] float speed = 9.5f;
+    [SerializeField] Camera camera;
     [SerializeField] GameObject bulletPrefab;
     [SerializeField] GameObject bulletSpawnPoint;
-    [SerializeField]GameObject ghostbulletPrefab;
-    [SerializeField] float bulletFireRate = 3f;
-    [SerializeField] float gostbulletFireRateDefoilt;
+    [SerializeField] GameObject ghostbulletPrefab;
+    [SerializeField] float bulletFireRateDefoilt = 6f;
+    float bulletFireRate = 3f;
+    [SerializeField] float ghostbulletFireRateDefoilt = 6f;
+     float gostbulletFireRate = 6f;
 
-   
-    
-     
-    
+
+
+
 
     void Update()
     {
         Move();
         Turn();
-        bulletFireRate -=  Time.deltaTime;
+        gostbulletFireRate -= Time.deltaTime;
+        if (gostbulletFireRate <= 0 && Input.GetMouseButtonDown(1))
+        {
+            Instantiate(ghostbulletPrefab, bulletSpawnPoint.transform.position, transform.rotation);
+            gostbulletFireRate = ghostbulletFireRateDefoilt;
+        }
+        bulletFireRate -= Time.deltaTime;
         if (bulletFireRate <= 0 && Input.GetMouseButtonDown(0))
-        {
-                Instantiate(bulletPrefab, bulletSpawnPoint.transform.position, transform.rotation);
-            bulletFireRate = 3f;
-        }
-        
 
-        
-        if (Input.GetMouseButtonDown(1))
         {
-            Instantiate(ghostbulletPrefab,bulletSpawnPoint.transform.position,transform.rotation);
+            Instantiate(bulletPrefab, bulletSpawnPoint.transform.position, transform.rotation);
+            bulletFireRate=bulletFireRateDefoilt;
         }
+
+
+
+
     }
-    
-        void Move()
-        {
-            float horizontal = Input.GetAxisRaw("Horizontal");
-            float vertical = Input.GetAxisRaw("Vertical");
-            transform.position = transform.position + new Vector3(horizontal, vertical) * Time.deltaTime * speed;
-        }
 
-        void Turn()
-        {
-            Vector2 mouseWorldPosition = camera.ScreenToWorldPoint(Input.mousePosition); 
+    void Move()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        transform.position = transform.position + new Vector3(horizontal, vertical) * Time.deltaTime * speed;
+    }
+
+    void Turn()
+    {
+        Vector2 mouseWorldPosition = camera.ScreenToWorldPoint(Input.mousePosition);
         Vector2 lookDirection = mouseWorldPosition - (Vector2)transform.position;
         float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 90f;
         transform.rotation = Quaternion.Euler(0, 0, angle);
-}
-
     }
+
+}
