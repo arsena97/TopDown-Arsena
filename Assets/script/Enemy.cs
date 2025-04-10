54d287597740e2eac61f3f14ce3ed3ea57bb9b2b
arsena97 <zivzivadzearsena72@gmail.com>
2025-04-10 20:07:31 +0400

    a

--- Assets/script/Enemy.cs
+++ Assets/script/Enemy.cs
@@ -1,8 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-   
+   [SerializeField] float speed = 3f;
+    Transform player;
+
+     void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        Debug.Log(player.name);
+       
+    }
+
+    void Update()
+    {
+
+        Vector3 direction = (player.position - transform.position).normalized;
+        transform.position += direction * speed * Time.deltaTime;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, angle);
+    }
+
+
+
 }
