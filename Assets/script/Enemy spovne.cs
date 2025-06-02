ea5c16e61755219741aa0f932e916f9a357f607a
arsena97 <zivzivadzearsena72@gmail.com>
2025-06-02 20:05:25 +0400

    a

--- Assets/script/Enemy spovne.cs
+++ Assets/script/Enemy spovne.cs
@@ -6,16 +6,21 @@ using UnityEngine;
 public class Enemyspovne : MonoBehaviour
 {
     [SerializeField] GameObject enemyPrafab;
-    float spawnRate = 2.5f;
-
+    [SerializeField] float initiallSpawnRate = 2f;
+    float spawnRate;
+     void Start()
+    {
+        spawnRate = initiallSpawnRate;
+    }
     void Update()
     {
-        spawnRate -=  Time.deltaTime;
+        spawnRate -= Time.deltaTime;
 
-     if (spawnRate <= 0)
+        if (spawnRate <= 0)
         {
-            Instantiate(enemyPrafab,transform.position, transform.rotation);
-            spawnRate = 2.5f;
+            Instantiate(enemyPrafab, transform.position, transform.rotation);
+            spawnRate = initiallSpawnRate;
+
 
         }
     }
